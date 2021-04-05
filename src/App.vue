d5b8015f456afdcc2176dab6d3f68f74db4d82e9
Alesson Marques <alessonmarques@live.com>
2021-04-04 23:51:05 -0300

    Added the ul element to display the peopleData, with li, returned from the API.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,16 @@
         <div class="py-12">
             <div class="bg-white shadow-xl sm:rounded-lg flex" style="min-height: 800px; max-height:800px;">
                 <div class="w-3/12 bg-gray-200 pl-5 bg-opacity-25 border-r border-gray-200 overflow-y-scroll">
-                   
+                   <ul>
+                        <li :class="(peopleActive && peopleActive.url == people.url) ? 'bg-gray-300 bg-opacity-50' : ''"
+                            v-for="people in peoples" :key="people.url"
+                            @click="() => {loadPeopleData(people.url)}"
+                            class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-opacity-50 hover:cursor-pointer hover:bg-gray-400">
+                            <p class="flex items-center">
+                                {{ people.name }}
+                            </p>
+                        </li>
+                    </ul>
                 </div>
                 <div class="w-10/12 flex flex-col justify-between">
                     <div class="w-full p-6 flex flex-col overflow-y-scroll h-full">
@@ -27,6 +36,7 @@
         data() {
             return {
                 peoples: [],
+                peopleActive: null,
             }
         },
         methods: {
