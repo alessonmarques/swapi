d0dbeb0d742efb80d8d1fb1ec1abee164465508d
Alesson Marques <alessonmarques@live.com>
2021-04-04 23:46:28 -0300

    Imported axios and loaded the initalData taking PeopleData from the API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template >
   <div id="app" >
-      <h2 class="font-semibold">The Star Wars API</h2>
+      <h2 class="font-semibold">The Star Wars</h2>
         <div class="py-12">
             <div class="bg-white shadow-xl sm:rounded-lg flex" style="min-height: 800px; max-height:800px;">
                 <div class="w-3/12 bg-gray-200 pl-5 bg-opacity-25 border-r border-gray-200 overflow-y-scroll">
@@ -17,20 +17,41 @@
 </template>
 
 <script>
+    import axios from '../node_modules/axios'
+    var api = 'https://swapi.dev/api';
+    
     export default {
         name: 'app',
         components: {
         },
+        data() {
+            return {
+                peoples: [],
+            }
+        },
+        methods: {
+            loadInititalData: async function ()
+            {
+                axios.get(`${api}/people/`).then( response => {
+                    console.log(response)
+                    this.peoples = response.data.results
+                });
+            },
+        },
+        mounted() 
+        {
+            this.loadInititalData();
+        }
     }
 </script>
 
 <style>
-    #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
